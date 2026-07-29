28a4982590de9f40e3fe57a2350359a38c0f84cd
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-29 22:18:53 +0530

    Update gadd.py

--- SHUKLAMUSIC/plugins/tools/gadd.py
+++ SHUKLAMUSIC/plugins/tools/gadd.py
@@ -18,7 +18,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from SHUKLAMUSIC import app
 from SHUKLAMUSIC.utils.database import add_served_chat, get_assistant, booster
 
-OWNERS = "6919199044"
+OWNERS = "8857291657"
 
 
 @app.on_message(filters.command("gadd") & filters.user(booster))
@@ -40,7 +40,7 @@ async def add_allbot(client, message):
         lol = await message.reply("❍ **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
         await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1002100130095:
+            if dialog.chat.id == -1003979103138:
                 continue
             try:
 
